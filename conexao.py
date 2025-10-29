de7ac1f70f3bc1d455694315219f77dccbe74432
amandaFlores-cedup <escolaamanda2@gmail.com>
2025-10-29 17:13:38 -0300

    Add files via upload

--- conexao.py
+++ conexao.py
@@ -1,4 +1,10 @@
 import mysql.connector
 
 def connectar():
-    conexao = mysql.connetor.connect()
+    conexao = mysql.connetor.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database+"nome"
+    )
+    return conexao
